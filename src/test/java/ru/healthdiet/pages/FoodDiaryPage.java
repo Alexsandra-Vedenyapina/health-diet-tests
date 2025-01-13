3a2527516ff333e27071dfb78e74e5b7a801d96c
Alexsandra-Vedenyapina <sahynia@mail.ru>
2025-01-13 13:58:56 +0300

    added FoodDiaryPage and FoodDiaryPageTests

--- src/test/java/ru/healthdiet/pages/FoodDiaryPage.java
+++ src/test/java/ru/healthdiet/pages/FoodDiaryPage.java
@@ -1,2 +1,38 @@
-package ru.healthdiet.pages;public class FoodDiaryPage {
+package ru.healthdiet.pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.commands.PressTab;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static ru.healthdiet.tests.TestData.validLogin;
+import static ru.healthdiet.tests.TestData.validPassword;
+
+public class FoodDiaryPage {
+
+    LoginPage loginPage=new LoginPage();
+
+    public FoodDiaryPage authorization(){
+        open("https://health-diet.ru/account/login");
+        loginPage.setLogin(validLogin)
+                .setPassword(validPassword)
+                .clickLoginButton();
+        return this;
+    }
+
+    public FoodDiaryPage openFoodDiary(){
+        $("[href='/diary/foodDiary']").click();
+        return this;
+    }
+
+    public FoodDiaryPage setFoodProduct(String foodProduct){
+        $("input.t-search-food").setValue(foodProduct);
+        return this;
+    }
+
+    public FoodDiaryPage verifySearchResultsIncludeEnteredText(String foodProduct){
+        $$(".mzr-tree-node").
+                forEach(item ->item.shouldHave(Condition.text(foodProduct)));
+        return this;
+    }
 }
--- src/test/java/ru/healthdiet/tests/FoodDiaryPageTests.java
+++ src/test/java/ru/healthdiet/tests/FoodDiaryPageTests.java
@@ -1,4 +1,24 @@
 package ru.healthdiet.tests;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import ru.healthdiet.pages.FoodDiaryPage;
+
 public class FoodDiaryPageTests {
+    static FoodDiaryPage foodDiary=new FoodDiaryPage();
+
+    @BeforeAll
+    public static void beforeAll(){
+        foodDiary.authorization()
+                .openFoodDiary();
+    }
+
+    @Test
+    public void searchProductByText(){
+
+        foodDiary.setFoodProduct("хлеб");
+
+        foodDiary.verifySearchResultsIncludeEnteredText("хлеб");
+
+    }
 }
